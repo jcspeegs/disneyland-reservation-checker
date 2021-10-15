a00dfe27f27896dbaa980008dc24cc9676dd5c3e
Justin Speegle <justin@speegs.com>
2021-10-15 13:59:17 -0700

    Update help message, add /help command

--- telegram_bot.py
+++ telegram_bot.py
@@ -15,11 +15,17 @@ from telegram.ext import (
 )
 
 
-def start(update: Update, context: CallbackContext) -> None:
+def print_help(update: Update, context: CallbackContext) -> None:
     ''' Sends explanation on how to use the bot'''
-    message = 'Hi, use /check <yyyy-mm-dd> to check reservations at' \
-        + 'Disneyland or California Adventure.'
-    update.message.reply_text(message)
+    name = update.effective_user
+    text = 'Disneyland Reservation Checker'
+    url = 'https://github.com/jcspeegs/disneyland-reservation-checker'
+    message = f'Hi {name.mention_markdown_v2()}, I am the ' \
+              f'{text}'\
+              f'\n``` /check <yyyy-mm-dd>```\nchecks reservations at ' \
+              f'Disneyland or California Adventure\.' \
+              f'\n``` /help```\nprints this help message\.'
+    update.message.reply_markdown_v2(message, disable_web_page_preview=False)
 
 
 def check(update: Update, context: CallbackContext) -> None:
@@ -47,12 +53,13 @@ def main():
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('start', print_help))
+    dispatcher.add_handler(CommandHandler('help', print_help))
     dispatcher.add_handler(CommandHandler('check', check))
 
     updater.start_polling()
     updater.idle()
 
 
-if __name__ ==  '__main__':
+if __name__ == '__main__':
     main()
