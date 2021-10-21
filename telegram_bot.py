78a43d4bfb3cf279146dd6648b345706685c658f
Justin Speegle <justin@speegs.com>
2021-10-21 12:09:03 -0700

    updates to telegram bot: reconfigure print help, generalize build_context, update list_jobs, use keyboard to remove jobs, add shorthand commands

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,31 +7,50 @@ import sys
 import argparse
 import yaml
 import logging.config
+from math import floor
 import bot_config
 import disneyland_reservation_checker as drc
-from telegram import Update
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
 from telegram.ext import (
     Updater,
     CallbackContext,
+    CallbackQueryHandler,
     CommandHandler,
 )
 
 
+DOC = {'CHECK': {'SYNTAX': '/check `<yyyy-mm-dd>`',
+                 'DESC': 'Check for reservations every 30 seconds\.'},
+       # 'CHECK1': {'SYNTAX': '`/check1 <yyyy-mm-dd>`',
+       #            'DESC': 'Isolated check for reservations at Disneyland ' \
+       #                     'or California Adventure\.'},
+       'LIST': {'SYNTAX': '/list',
+                'DESC': 'List query queue\.'},
+       'DEL': {'SYNTAX': '/del',
+               'DESC': 'Remove job from query queue\.'},
+       'HELP': {'SYNTAX': '/help',
+                'DESC': 'Print this help message\.'}
+       }
+
+
 def print_help(update: Update, context: CallbackContext) -> None:
     ''' Sends explanation on how to use the bot'''
+
+    logger = logging.getLogger(__name__)
+    logger.info('Printing help message.')
+
     name = update.effective_user
     text = 'Disneyland Reservation Checker'
     url = 'https://github.com/jcspeegs/disneyland-reservation-checker'
     message = f'Hi {name.mention_markdown_v2()}, I am the ' \
-              f'[{text}]({url})' \
-              f'\n``` /check <yyyy-mm-dd>```\nChecks for reservations at ' \
-              r'Disneyland or California Adventure every 30 seconds\.' \
-              f'\n``` /check1 <yyyy-mm-dd>```\nIsolated check for '\
-              r'reservations at Disneyland or California Adventure\.' \
-              f'\n``` /list```\nList query queue' r'\.' \
-              f'\n``` /del <job-number>```\nRemove job from query queue' \
-              r'\.' \
-              f'\n``` /help```\nPrints this help message' r'\.'
+              f'[{text}]({url})\n'
+    for d in DOC.values():
+        message += f'\n {d["SYNTAX"]}\n{d["DESC"]}'
+
     update.message.reply_markdown_v2(message, disable_web_page_preview=True)
 
 
@@ -42,8 +61,7 @@ def query(context: CallbackContext) -> None:
     check_date = context.job.context['check_date']
     chat_id = context.job.context['chat_id']
 
-    calendar = \
-        drc.DisneylandReservationChecker(check_date)
+    calendar = drc.DisneylandReservationChecker(check_date)
     calendar.refresh()
     calendar.validate()
     message = str(calendar)
@@ -52,12 +70,19 @@ def query(context: CallbackContext) -> None:
     context.bot.send_message(chat_id, message)
 
 
-def build_context(update: Update, context: CallbackContext):
+def build_context(update: Update, context: CallbackContext, chat_id=False,
+                  check_date=False):
     ''' Build context dictionary inteded to be passed to query'''
 
+    logger = logging.getLogger(__name__)
+
     job_context = {}
-    job_context['chat_id'] = update.message.chat_id
-    job_context['check_date'] = context.args[0]
+    if chat_id:
+        job_context['chat_id'] = update.message.chat_id
+        logger.debug(f'chat_id:{job_context["chat_id"]}')
+    if check_date:
+        job_context['check_date'] = context.args[0]
+        logger.debug(f'check_date:{job_context["check_date"]}')
 
     return job_context
 
@@ -67,9 +92,10 @@ def check1(update: Update, context: CallbackContext) -> None:
 
     # Get logger
     logger = logging.getLogger(__name__)
+    logger.info('Running Check1')
 
     # Build job context to pass to query function
-    job_context = build_context(update, context)
+    job_context = build_context(update, context, chat_id=True, check_date=True)
     logger.debug(f'job_context:{job_context}')
     context.job_queue.run_once(query, 0, context=job_context,
                                name=str(job_context['chat_id']))
@@ -80,9 +106,10 @@ def keep_checking(update: Update, context: CallbackContext) -> None:
 
     # Get logger
     logger = logging.getLogger(__name__)
+    logger.info('Running keep_checking')
 
     # Build job context to pass to query function
-    job_context = build_context(update, context)
+    job_context = build_context(update, context, chat_id=True, check_date=True)
     logger.debug(f'job_context:{job_context}')
 
     context.job_queue.run_repeating(query, interval=30, first=0.1,
@@ -104,22 +131,45 @@ def list_jobs(update: Update, context: CallbackContext) -> None:
     logger.info('Listing jobs')
 
     # Get active jobs
-    name = str(update.message.chat_id)
+    job_context = build_context(update, context, chat_id=True)
+    name = str(job_context['chat_id'])
     logger.debug(f'name:{name}:{type(name)}')
     jobs = get_active_jobs(context, name)
-    logger.debug(f'jobs:{jobs}')
+    job_count = len(jobs)
     logger.debug(f'active jobs:{len(jobs)}')
 
     # Print list of active jobs
-    message = []
-    for i, j in enumerate(jobs):
-        logger.debug(f'context:{j.context}')
-        logger.debug(f'name:{j.name}')
-        desc = f'{i}) Checking {j.context["check_date"]}'
-        message.append(desc)
-    message = '\n'.join(message)
+    if job_count > 0:
+        message = []
+        for i, j in enumerate(jobs):
+            desc = f'{i}) Checking {j.context["check_date"]}'
+            message.append(desc)
+        message = '\n'.join(message)
+        logger.debug(f'message:{message}')
+        update.message.reply_text(message)
 
-    update.message.reply_text(message)
+    elif job_count == 0:
+        syntax = DOC['CHECK']['SYNTAX']
+        message = f'No dates to check\.\nAdd one with: {syntax}'
+        context.bot.send_message(name, message, parse_mode='MarkdownV2')
+
+
+def define_keyboard(jobs: tuple, columns: int) -> list:
+    ''' Create inline keyboard structure.  Returns a list of lists where each
+        internal list represents a row and each item in the interior list
+        represents a column.
+        e.g. [[r1c1, r1c2], [r2c1, r2c2]]
+    '''
+
+    jk = [InlineKeyboardButton(j.context['check_date'], callback_data=i)
+          for i, j in enumerate(jobs)]
+    job_keyboard = []
+    for i, j in enumerate(jk):
+        if i % columns == 0:
+            job_keyboard.append([])
+        job_keyboard[floor(i/columns)].append(j)
+
+    return job_keyboard
 
 
 def del_job(update: Update, context: CallbackContext) -> None:
@@ -129,19 +179,53 @@ def del_job(update: Update, context: CallbackContext) -> None:
     logger = logging.getLogger(__name__)
     logger.info('Deleting a job')
 
-    # Get index of job to end
     # Get active jobs
-    name = str(update.message.chat_id)
+    job_context = build_context(update, context, chat_id=True)
+    name = str(job_context['chat_id'])
     logger.debug(f'name:{name}:{type(name)}')
     jobs = get_active_jobs(context, name)
+    job_count = len(jobs)
     logger.debug(f'jobs:{jobs}')
-    logger.debug(f'active jobs:{len(jobs)}')
-    job = jobs[int(context.args[0])]
+    logger.debug(f'active jobs:{job_count}')
+
+    # Get index of job to end
+    if job_count > 0:
+        keyboard = define_keyboard(jobs, columns=2)
+        logger.debug(f'keyboard:{keyboard}')
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text('Choose date to remove:',
+                                  reply_markup=reply_markup)
+    elif job_count == 0:
+        update.message.reply_text('No jobs to remove')
+
+
+def button_press(update: Update, context: CallbackContext) -> None:
+    ''' Parse the CallbackQuery when inline keyboard button
+        is pressed'''
+
+    # Get logger
+    logger = logging.getLogger(__name__)
+    logger.info('Button Press')
+
+    query = update.callback_query
+    logger.debug(f'query:{query}')
+    logger.debug(f'query.data:{query.data}:type:{type(query.data)}')
+    query.answer(text=f'Stopping check {query.data}')
+    # query.edit_message_text(text=f'Select options: {query.data}')
+
+    name = str(query.message.chat.id)
+    logger.debug(f'name:{name}')
+    jobs = get_active_jobs(context, name)
+    logger.debug(f'jobs:{jobs}')
+    job = jobs[int(query.data)]
+    logger.debug(f'job:{job}')
     logger.debug(f'Delete job:{job}')
     try:
         job.schedule_removal()
     except Exception:
         raise
+
+    query.edit_message_text(text='Job removed')
 
 
 def parse_arguments(args):
@@ -180,10 +264,15 @@ def main():
 
     dispatcher.add_handler(CommandHandler('start', print_help))
     dispatcher.add_handler(CommandHandler('help', print_help))
+    dispatcher.add_handler(CommandHandler('h', print_help))
     dispatcher.add_handler(CommandHandler('check1', check1))
     dispatcher.add_handler(CommandHandler('check', keep_checking))
+    dispatcher.add_handler(CommandHandler('c', keep_checking))
     dispatcher.add_handler(CommandHandler('list', list_jobs))
+    dispatcher.add_handler(CommandHandler('l', list_jobs))
     dispatcher.add_handler(CommandHandler('del', del_job))
+    dispatcher.add_handler(CommandHandler('d', del_job))
+    dispatcher.add_handler(CallbackQueryHandler(button_press))
 
     updater.start_polling()
     updater.idle()
